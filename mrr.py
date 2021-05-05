b8b2b07176f5f24c5a5cea8df591c5aa95cecde8
Theodosis Siomos <30955712+theodosissio@users.noreply.github.com>
2021-05-05 11:37:41 +0300

    Update mrr.py

--- mrr.py
+++ mrr.py
@@ -16,7 +16,7 @@ class MeanReciprocalRank(tf.keras.metrics.Metric):
         self.total = self.add_weight('total', initializer='zeros')
         self.count = self.add_weight('count', initializer='zeros')
 
-    def update_state(self, y_true, y_pred):
+    def update_state(self, y_true, y_pred, sample_weight=None):
         metric = self.mrr_fn(y_true, y_pred)
         self.total.assign_add(tf.cast(tf.reduce_sum(metric), dtype=tf.float32))
         self.count.assign_add(tf.cast(tf.size(y_true), dtype=tf.float32))
